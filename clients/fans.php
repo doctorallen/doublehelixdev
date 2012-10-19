21513b20351c7bcd3e3afb3a49217c483ee7b3d3
David Allen <drallen1@plymouth.edu>
2012-10-19 14:33:05 +0000

    added clients

---
+++ clients/fans.php
@@ -0,0 +1,6 @@
+<h1>Fans International</h1>
+<p>Fans International was our first client..
+Fans International
+Fans
+Fans
+</p>
--- work.php
+++ work.php
@@ -1,12 +1,12 @@
 <div class="work">
 	<span class="work-title">Fans International</span>
-	<a href="clients/fans.php"><img class="thumb" src="images/logo-150.png"></a>
+	<a href="clients/fans"><img class="thumb" src="images/logo-150.png"></a>
 </div>
 <div class="work">
 	<span class="work-title">Fans International</span>
-	<a href="clients/fans.php"><img class="thumb" src="images/logo-150.png"></a>
+	<a href="clients/fans"><img class="thumb" src="images/logo-150.png"></a>
 </div>
 <div class="work">
 	<span class="work-title">Fans International</span>
-	<a href="clients/fans.php"><img class="thumb" src="images/logo-150.png"></a>
+	<a href="clients/fans"><img class="thumb" src="images/logo-150.png"></a>
 </div>
