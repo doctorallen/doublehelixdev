27a1f1746b6fc579a0c3e647a3d9d415f62190a2
David Allen <drallen1@plymouth.edu>
2012-10-22 21:49:33 +0000

    added services to styling

--- services.php
+++ services.php
@@ -1,12 +1,18 @@
+<div class="service">
 <h2>Service Structure</h2>
 <p>We offer a few services, domain registration, website hosting, website design, and website development. Below is an explanation of what services we offer, and what you recieve with each one.</p>
+</div>
+
+<div class="service">
 <h2 id="domain">Domain Registration</h2>
 <p>Domain registration services include the following:</p>
 	<ul>
 		<li>Finding a great domain name that will suit your business name and model</li>
 		<li>Registiring a domain name and pointing that domain to the server your website is hosted to</li>
 	</ul>
+</div>
 
+<div class="service">
 <h2 id="hosting">Website Hosting</h2>
 <p>Website hosting services include the following:</p>
 	<ul>
@@ -15,7 +21,9 @@
 		<li>Server Maintenance and Backups</li>
 		<li>Email Accounts</li>
 	</ul>
+</div>
 
+<div class="service">
 <h2 id="design">Website Design</h2>
 <p>Website design services include the following:</p>
 	<ul>
@@ -23,14 +31,18 @@
 		<li>Implement design to development ready standards</li>
 		<li>Work with client to create the best suited design for the company</li>
 	</ul>
+</div>
 
+<div class="service">
 <h2 id="development">Website Development</h2>
 <p>Website development services include the following:</p>
 	<ul>
 		<li>Developing the code for the website</li>
 		<li>Implementing already existing design, or implement new design</li>
 	</ul>
+</div>
 
+<div class="service">
 <h2>Additional Services</h2>
 	<ul>
 		<li>Search Engine Optimization</li>
@@ -40,3 +52,4 @@
 		<li>Database Server Hosting on our Cloud Server</li>
 		<li>Database Server Backups</li>
 	</ul>
+</div>
