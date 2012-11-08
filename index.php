c1eeb0a525685d06b730f0665a3c27363b066526
David Allen <drallen1@plymouth.edu>
2012-11-08 16:23:38 +0000

    added new design branch

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<div class ="span9">
 			<div class="hero-unit">
 				<h1>What do we do?</h1>
-				<p>We make beautiful websites for small businesses.</p>
+				<p>We make redesigned websites for small businesses.</p>
 				<a href="/services" class="btn btn-primary">Learn More</a>
 				<a href="/work" class="btn btn-success">Like What?</a>
 				<a href="/contact" class="btn btn-info">I want one!</a>
