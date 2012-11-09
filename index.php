05b8dfa395dc99b7275596d1d093e182fad799c1
David Allen <drallen1@plymouth.edu>
2012-11-09 18:43:08 +0000

    added btn css

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <div class="hero-unit medium">
 	<h1 class="light">What do we do?</h1>
 	<p>We make beautiful websites for small businesses.</p>
-	<a href="/services" class="btn btn-primary">Learn More</a>
+	<a href="/services" class="btn btn-warning">Learn More</a>
 	<a href="/work" class="btn btn-success">Like What?</a>
 	<a href="/contact" class="btn btn-info">I want one!</a>
 </div>
