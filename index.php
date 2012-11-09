459c4c8459908ca88d6f63b0e33d4c43f728507d
David Allen <drallen1@plymouth.edu>
2012-11-09 22:16:23 +0000

    responsive

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <div class="hero-unit medium">
-	<h1 class="light">What do we do?</h1>
+	<h1 class="light-header">What do we do?</h1>
 	<p>We make beautiful websites for small businesses.</p>
 	<a href="/services" class="btn btn-warning">Learn More</a>
 	<a href="/work" class="btn btn-success">Like What?</a>
