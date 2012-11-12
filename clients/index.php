d8fa1c324230b75d1026054bf534a9a32148e2b9
David Allen <drallen1@plymouth.edu>
2012-11-12 14:12:13 +0000

    added breadcrumb and work styles

--- clients/index.php
+++ clients/index.php
@@ -10,3 +10,15 @@
 	<span class="work-title">Fans International</span>
 	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
 </div>
+<div class="work">
+	<span class="work-title">Fans International</span>
+	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
+</div>
+<div class="work">
+	<span class="work-title">Fans International</span>
+	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
+</div>
+<div class="work">
+	<span class="work-title">Fans International</span>
+	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
+</div>
