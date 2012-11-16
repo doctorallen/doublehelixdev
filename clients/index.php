94f71496ef22d90838ba4cbfa475fa93e2e62372
David Allen <drallen1@plymouth.edu>
2012-11-16 16:25:43 +0000

    changed client page

--- clients/index.php
+++ clients/index.php
@@ -1,24 +1,38 @@
-<div class="work">
-	<span class="work-title">Fans International</span>
-	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
-</div>
-<div class="work">
-	<span class="work-title">Fans International</span>
-	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
-</div>
-<div class="work">
-	<span class="work-title">Fans International</span>
-	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
-</div>
-<div class="work">
-	<span class="work-title">Fans International</span>
-	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
-</div>
-<div class="work">
-	<span class="work-title">Fans International</span>
-	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
-</div>
-<div class="work">
-	<span class="work-title">Fans International</span>
-	<a href="fans"><img class="thumb" src="../images/logo-150.png"></a>
+<div class="clients">
+	<a href="fans" class="work">
+		<div class="client">
+			<span class="work-title">Fans International</span>
+			<img class="thumb" src="../images/logo-150.png">
+		</div>
+	</a>
+	<a href="fans" class="work">
+		<div class="client">
+			<span class="work-title">Fans International</span>
+			<img class="thumb" src="../images/logo-150.png">
+		</div>
+	</a>
+	<a href="fans" class="work">
+		<div class="client">
+			<span class="work-title">Fans International</span>
+			<img class="thumb" src="../images/logo-150.png">
+		</div>
+	</a>
+	<a href="fans" class="work">
+		<div class="client">
+			<span class="work-title">Fans International</span>
+			<img class="thumb" src="../images/logo-150.png">
+		</div>
+	</a>
+	<a href="fans" class="work">
+		<div class="client">
+			<span class="work-title">Fans International</span>
+			<img class="thumb" src="../images/logo-150.png">
+		</div>
+	</a>
+	<a href="fans" class="work">
+		<div class="client">
+			<span class="work-title">Fans International</span>
+			<img class="thumb" src="../images/logo-150.png">
+		</div>
+	</a>
 </div>
