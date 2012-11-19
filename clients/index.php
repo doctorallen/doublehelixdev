f8bae5180796b74ea058b1b2832407643cbcfc30
David Allen <drallen1@plymouth.edu>
2012-11-19 15:03:16 +0000

    changed titles on client items

--- clients/index.php
+++ clients/index.php
@@ -1,38 +1,40 @@
 <div class="clients">
 	<a href="fans" class="work">
 		<div class="client">
-			<span class="work-title">Fans International</span>
 			<img class="thumb" src="../images/logo-150.png">
+			<div class="clients-header none">
+				<h1 class="work-title">Fans International</h1>
+			</div>
 		</div>
 	</a>
 	<a href="fans" class="work">
 		<div class="client">
-			<span class="work-title">Fans International</span>
 			<img class="thumb" src="../images/logo-150.png">
+			<div class="clients-header none">
+				<h1 class="work-title">Fans Stuff</h1>
+			</div>
 		</div>
 	</a>
 	<a href="fans" class="work">
 		<div class="client">
-			<span class="work-title">Fans International</span>
 			<img class="thumb" src="../images/logo-150.png">
-		</div>
-	</a>
-	<a href="fans" class="work">
-		<div class="client">
-			<span class="work-title">Fans International</span>
-			<img class="thumb" src="../images/logo-150.png">
-		</div>
-	</a>
-	<a href="fans" class="work">
-		<div class="client">
-			<span class="work-title">Fans International</span>
-			<img class="thumb" src="../images/logo-150.png">
-		</div>
-	</a>
-	<a href="fans" class="work">
-		<div class="client">
-			<span class="work-title">Fans International</span>
-			<img class="thumb" src="../images/logo-150.png">
+			<div class="clients-header none">
+				<h1 class="work-title">Fans Stuffer</h1>
+			</div>
 		</div>
 	</a>
 </div>
+<script>
+	$(document).ready(function(){
+		$('.client').mouseenter( function() {
+			var title = $(this).children('.clients-header');
+			$(title).fadeIn(200);
+			$(title).removeClass('none');
+		});
+		$('.client').mouseleave( function() {
+			var title = $(this).children('.clients-header');
+			$(title).fadeOut();
+			$(title).addClass('none');
+		});
+	});
+</script>
