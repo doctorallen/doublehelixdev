c00cce0c9d4e511232ffaf69a407e6b7fe4cf85e
David Allen <drallen1@plymouth.edu>
2013-01-27 04:20:48 +0000

    added 3d button

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-<div class="hero-unit medium">
-	<h1 class="light-header">What do we do?</h1>
-	<p>We make beautiful websites for small businesses.</p>
-	<a href="/services" class="btn btn-warning">Learn More</a>
-	<a href="/work" class="btn btn-success">Like What?</a>
-	<a href="/contact" class="btn btn-info">I want one!</a>
-</div>
+<h1>We build beautiful websites for small businesses.</h1>
+<a href="/services" class="btn-3d">Work with us</a>
