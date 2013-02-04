394d6edff640553adb3505702a0932fdddfcc53e
David Allen <drallen1@plymouth.edu>
2013-02-04 19:08:11 +0000

    added trevor page

---
+++ trevor.php
@@ -0,0 +1,2 @@
+<h1 class="trevor">Happy Birthday <span class="trevor orange">Trevor</span> <span class="trevor blue">Suarez</> :)</h1>
+<a href="https://www.facebook.com/tnsuarez" class="btn-3d">Awesomeness</a>
